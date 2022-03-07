c28ac5b86b6c4492471f6213c9fd3a711115e2e5
sanchezcode <sanchezcode7@gmail.com>
2022-03-07 16:06:08 -0500

    Update 07/03/22

--- frontend/src/app/board/list-task/list-task.component.ts
+++ frontend/src/app/board/list-task/list-task.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskService } from "../../services/task.service";
+import { TaskService } from '../../services/task.service';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list-task',
   templateUrl: './list-task.component.html',
-  styleUrls: ['./list-task.component.css']
+  styleUrls: ['./list-task.component.css'],
 })
 export class ListTaskComponent implements OnInit {
+  taskData: any;
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2000;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _taskService: TaskService,
+    private _snackBar: MatSnackBar
+  ) {
+    this.taskData = {};
   }
 
+  ngOnInit(): void {}
+  updateTask(task: any, status: string) {}
+  deleteTask(task: any) {}
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds,
+      panelClass: ['styleSnackBarSuccesfull'],
+    });
+  }
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds,
+      panelClass: ['styleSnackBarError'],
+    });
+  }
 }
--- frontend/src/app/services/task.service.ts
+++ frontend/src/app/services/task.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private env: string;
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    this.env = environment.APP_URL;
+  }
+
+  saveTask(task: any) {
+    return this._http.post<any>(this.env + 'task/save', task);
+  }
+
+  listTask() {
+    return this._http.get<any>(this.env + 'task/list');
+  }
 }
